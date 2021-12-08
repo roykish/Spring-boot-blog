cfe412fb72558c8654acb6a9fdda767f0ab8f1d2
roykish <roykishor93@gmail.com>
2021-12-08 13:26:18 +0600

    Added security 1st commit

--- src/main/java/com/spring/boot/blog/Springbootblog/dto/PostDto.java
+++ src/main/java/com/spring/boot/blog/Springbootblog/dto/PostDto.java
@@ -2,6 +2,9 @@ package com.spring.boot.blog.Springbootblog.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.stream.Collector;
 
--- src/main/java/com/spring/boot/blog/Springbootblog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/spring/boot/blog/Springbootblog/exceptions/GlobalExceptionHandler.java
@@ -1,20 +1,24 @@
 package com.spring.boot.blog.Springbootblog.exceptions;
 
 import com.spring.boot.blog.Springbootblog.dto.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @ControllerAdvice //As this is a global exception handler, we need to annotate it @ControllerAdvice.
 
-/*
-GlobalExceptionHandler handles all the exceptions.
- */
-public class GlobalExceptionHandler {
+
+//GlobalExceptionHandler handles all the exceptions.
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
     /*
     Every method, handling an exception, should be annotated by @ExceptionHandler
@@ -34,11 +38,13 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
     //GlobalExceptionHandler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 
 }
