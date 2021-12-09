02306ec96e78f93ba0e1d20af66589dca71f7d24
roykish <roykishor93@gmail.com>
2021-12-09 16:40:42 +0600

    71, 3:40

--- src/main/java/com/spring/boot/blog/Springbootblog/dao/UserRepository.java
+++ src/main/java/com/spring/boot/blog/Springbootblog/dao/UserRepository.java
@@ -1,4 +1,14 @@
 package com.spring.boot.blog.Springbootblog.dao;
 
-public interface UserRepository {
+import com.spring.boot.blog.Springbootblog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean isExistByUsername(String username);
+    Boolean isExistByEmail(String email);
 }
---
+++ src/main/java/com/spring/boot/blog/Springbootblog/security/CustomUserDetailService.java
@@ -0,0 +1,35 @@
+package com.spring.boot.blog.Springbootblog.security;
+
+import com.spring.boot.blog.Springbootblog.dao.UserRepository;
+import com.spring.boot.blog.Springbootblog.entity.Role;
+import com.spring.boot.blog.Springbootblog.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+//there is a builtin interface provided by spring boot named UserDetailService.
+//So we're making our own new customUserDetailService here.
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+       User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(()-> new UsernameNotFoundException("user is not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
+        return roles.stream().map(role->new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
+    }
+
+
+}
